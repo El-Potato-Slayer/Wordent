b38dcfe7993d6b039d996447564d38b73bd5c80d
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-01-19 20:13:47 +0200

    added navbar dropdown

--- app/javascript/packs/methods.js
+++ app/javascript/packs/methods.js
@@ -1,17 +1,54 @@
-function myFunction(target) { 
-  // let x = document.getElementById("nav-links")
-  // if (x.style.display === "flex") {
-  //   x.style.display = "none"
-  // } else {
-  //   x.style.display = "flex"
-  // }
-  alert('sd')
-}
-
 document.addEventListener('turbolinks:load', () => {  
-  const clickButton = document.getElementById("nav-btn");  
-  let target = clickButton.querySelector('data-target="#nav-links"')
-  clickButton.addEventListener("click", () => {
-    alert('dfd')
+  const clickButton = document.getElementById("nav-btn");
+  let target = document.getElementById("nav-links")
+  clickButton.addEventListener("click", function () {
+    // if (target.style.) {
+      
+    // }
+    // alert(document.documentElement.clientWidth)
+    // $(target).toggleClass("flex")
+    if (target.style.display === "none" && document.documentElement.clientWidth < 1024) {
+      
+    }
+    $(target).animate({
+      height:"toggle"
+    })
+    // if (screen.width < 1024) {
+    // }
   }); 
+
+  window.addEventListener("resize", function () {
+    if (document.documentElement.clientWidth >= 1024) {
+      target.style.display = "flex"
+    } else {
+      target.style.display = "none"
+    }
+  })
 });
+
+// document.addEventListener('turbolinks:load', () => {  
+//   const clickButton = document.getElementById("nav-btn");  
+//   // let target = clickButton.querySelector('[data-target="#nav-links"]')
+//   // alert('ds')
+//   let target = document.getElementById("nav-links")
+//   clickButton.addEventListener("click", function () {
+//     if (target.style.display === "flex") {
+//       target.style.display = "none"
+//     } else {
+//       target.style.display = "flex"
+//     }
+//   }); 
+// });
+
+// document.addEventListener('turbolinks:load', () => {
+//   $(document).ready(function () {
+//     $(".nav-btn").each(function (_, navToggler) {
+//       var target = $(navToggler).data("nav-links")
+//       $(navToggler).on("click", function() {
+//         $(target).animate({
+//           height:"toggle"
+//         })
+//       })
+//     })
+//   })
+// });
--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -350,6 +350,7 @@ module.exports = {
     height: (theme) => ({
       auto: 'auto',
       ...theme('spacing'),
+      '0': '0%',
       '1/2': '50%',
       '1/3': '33.333333%',
       '2/3': '66.666667%',
