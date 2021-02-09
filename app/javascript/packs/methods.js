d2e1b68b8d4d404d1e6f19b879b07f85956708a7
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-02-09 19:16:59 +0200

    fixed article form bugs and added padding to page title

--- app/javascript/packs/methods.js
+++ app/javascript/packs/methods.js
@@ -21,50 +21,24 @@ document.addEventListener('turbolinks:load', () => {
     reader.readAsDataURL(image);
     console.log(files);
   });
-  // });
-  
-  // let likeButton = document.getElementById("like")
+
   $('#like').on("click", function () {
     $("#like").load(" #like > *");
   })
 
-  // function auto_grow(element) {
-  //   element.style.height = "5px";
-  //   element.style.height = (element.scrollHeight)+"px";
-  // }
+  $('textarea').load( "textarea", function () {
+    $(this).height(1);
+    setTextAreaHeight($(this))
+  })
 
-  // txt = document.getElementById("txt")
-  
-  // auto_grow(txt)
   $('textarea').on('keydown', function(e){
     if(e == 13) {e.preventDefault();}
   }).on('input', function(){
-      $(this).height(1);
-      var totalHeight = $(this).prop('scrollHeight') - parseInt($(this).css('padding-top')) - parseInt($(this).css('padding-bottom'));
-      $(this).height(totalHeight);
+      setTextAreaHeight($(this))
   });
-//   var span = $('<span>').css('display','inline-block')
-//                       .css('word-break','break-all')
-//                       .appendTo('body').css('visibility','hidden');
-// function initSpan(textarea){
-//   span.text(textarea.text())
-//       .width(textarea.width())
-//       .css('font',textarea.css('font'));
-// }
-// $('textarea').on({
-//     input: function(){
-//        var text = $(this).val();      
-//        span.text(text);      
-//        $(this).height(text ? span.height() : '1.1em');
-//     },
-//     focus: function(){           
-//        initSpan($(this));
-//     },
-//     keypress: function(e){
-//        //cancel the Enter keystroke, otherwise a new line will be created
-//        //This ensures the correct behavior when user types Enter 
-//        //into an input field
-//        if(e.which == 13) e.preventDefault();
-//     }
-// });
+
+  function setTextAreaHeight(textArea) {
+    var totalHeight = $(textArea).prop('scrollHeight') - parseInt($(textArea).css('padding-top')) - parseInt($(textArea).css('padding-bottom'));
+    $(textArea).height(totalHeight);
+  }
 });
