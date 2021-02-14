c4d66a211d5d3104ed1421c304e056463902e999
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-02-14 22:37:45 +0200

    migrated from sqlite to postgresql

--- db/migrate/20210115180110_create_articles.rb
+++ db/migrate/20210115180110_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :text
+      t.text :description
       t.integer :author_id, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_004415) do
+ActiveRecord::Schema.define(version: 2021_02_06_170437) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 2021_02_07_004415) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -45,14 +48,14 @@ ActiveRecord::Schema.define(version: 2021_02_07_004415) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "article_categories", force: :cascade do |t|
-    t.integer "article_id", null: false
-    t.integer "category_id", null: false
+    t.bigint "article_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_article_categories_on_article_id"
@@ -88,8 +91,8 @@ ActiveRecord::Schema.define(version: 2021_02_07_004415) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "article_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_votes_on_article_id"
